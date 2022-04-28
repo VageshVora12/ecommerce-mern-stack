1771c3778f9c8cd1d1b6c21100f78cd27fd04cdd
vagesh <vageshvora12@gmail.com>
2022-04-29 00:30:36 +0530

    minor changes

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="https://media-exp1.licdn.com/dms/image/C4E03AQHEs27WQv_FyQ/profile-displayphoto-shrink_200_200/0/1637565911202?e=1652918400&v=beta&t=XkofJ5Cyd8jbD8R3eg2O7WvdbfztYmIJsOQsXOmwQgk"
               alt="Founder"
             />
             <Typography>Vagesh Vora</Typography>
@@ -33,7 +33,7 @@ const About = () => {
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://www.youtube.com/"
               target="blank"
             >
               <YouTubeIcon className="youtubeSvgIcon" />
